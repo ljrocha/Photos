3fa4a9b7b5bad279e424bfa355feb17d50a2a6b0
ljrocha <ljrocha@users.noreply.github.com>
2020-12-05 16:51:33 -0800

    Update pod specification file. Update git ignore file.

---
+++ Photos/Private.swift
@@ -0,0 +1,12 @@
+//
+//  Private.swift
+//  Photos
+//
+//  Created by Leandro Rocha on 12/5/20.
+//
+
+import Foundation
+
+struct Private {
+    static let accessKey = "pIinbrT-NE-EzvMHkPxBzivGzP5iddPqqfaOZWegPng"
+}
